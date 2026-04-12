8cf57fca6e64733ec8f0106d353643a18003630f
Janmejai Mudgal <janmejai345@gmail.com>
2026-04-12 05:47:45 +0000

    feat: broadcast updates to connected users

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -1,45 +1,25 @@
 import { WebSocketServer } from "ws";
 
-const rooms: Record<string, Set<any>> = {};
+const clients = new Set<any>();
 
 export const setupWebSocket = (server: any) => {
   const wss = new WebSocketServer({ server });
 
   wss.on("connection", (ws) => {
-    let currentRoom = "";
+    clients.add(ws);
 
     ws.on("message", (message) => {
-      const data = JSON.parse(message.toString());
+      const data = message.toString();
 
-      if (data.type === "join") {
-        currentRoom = data.room;
-
-        if (!rooms[currentRoom]) {
-          rooms[currentRoom] = new Set();
+      clients.forEach((client) => {
+        if (client !== ws) {
+          client.send(data);
         }
-
-        rooms[currentRoom].add(ws);
-      }
-
-      if (data.type === "code-change") {
-        rooms[currentRoom]?.forEach((client) => {
-          if (client !== ws) {
-            client.send(JSON.stringify(data));
-          }
-        });
-      }
+      });
     });
 
     ws.on("close", () => {
-      if (currentRoom && rooms[currentRoom]) {
-        rooms[currentRoom].delete(ws);
-
-        if (rooms[currentRoom].size === 0) {
-          delete rooms[currentRoom];
-        }
-      }
-
-      console.log("Client disconnected");
+      clients.delete(ws);
     });
   });
 };
