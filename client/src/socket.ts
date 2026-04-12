302640f2ff2aeaa9b08be87163103a3aa15c804d
Janmejai Mudgal <janmejai345@gmail.com>
2026-04-12 05:48:27 +0000

    refactor: optimize socket event handling

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,15 +1,10 @@
-let editorRef: any;
-
-export const setEditor = (editor: any) => {
-  editorRef = editor;
-};
-
 export const socket = new WebSocket("ws://localhost:5000");
 
-socket.onmessage = (event) => {
-  const data = JSON.parse(event.data);
-
-  if (editorRef && data.type === "code-change") {
-    editorRef.setValue(data.code);
-  }
+export const sendCodeChange = (code: string) => {
+  socket.send(
+    JSON.stringify({
+      type: "code-change",
+      code,
+    })
+  );
 };
