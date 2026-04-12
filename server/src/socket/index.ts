8558769aa23f29de239ede1645703f2bf82a1e8d
Janmejai Mudgal <janmejai345@gmail.com>
2026-04-12 05:59:54 +0000

    feat: track connected users per room

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -1,25 +1,66 @@
 import { WebSocketServer } from "ws";
 
-const clients = new Set<any>();
+type Client = {
+  ws: any;
+  username: string;
+  color: string;
+};
+
+const rooms: Record<string, Set<Client>> = {};
+
+const randomName = () => "User" + Math.floor(Math.random() * 1000);
+const randomColor = () =>
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
 
 export const setupWebSocket = (server: any) => {
   const wss = new WebSocketServer({ server });
 
   wss.on("connection", (ws) => {
-    clients.add(ws);
+    let currentRoom = "";
+    let client: Client;
 
     ws.on("message", (message) => {
-      const data = message.toString();
+      const data = JSON.parse(message.toString());
 
-      clients.forEach((client) => {
-        if (client !== ws) {
-          client.send(data);
+      if (data.type === "join") {
+        currentRoom = data.room;
+
+        if (!rooms[currentRoom]) {
+          rooms[currentRoom] = new Set();
         }
-      });
+
+        client = {
+          ws,
+          username: randomName(),
+          color: randomColor(),
+        };
+
+        rooms[currentRoom].add(client);
+
+        const users = Array.from(rooms[currentRoom]).map((c) => ({
+          username: c.username,
+          color: c.color,
+        }));
+
+        rooms[currentRoom].forEach((c) => {
+          c.ws.send(
+            JSON.stringify({
+              type: "users",
+              users,
+            })
+          );
+        });
+      }
     });
 
     ws.on("close", () => {
-      clients.delete(ws);
+      if (currentRoom && rooms[currentRoom]) {
+        rooms[currentRoom].forEach((c) => {
+          if (c.ws === ws) {
+            rooms[currentRoom].delete(c);
+          }
+        });
+      }
     });
   });
 };
