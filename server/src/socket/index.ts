4fc35251f9e7cb12f040abba75ba7ae8e7a06d6a
Janmejai Mudgal <janmejai345@gmail.com>
2026-04-14 17:05:38 +0000

    feat: broadcast cursor movement via sockets

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -1,70 +1,27 @@
 import { WebSocketServer } from "ws";
 
-type Client = {
-  ws: any;
-  username: string;
-  color: string;
-};
-
-const rooms: Record<string, Set<Client>> = {};
-
-const randomName = () => "User" + Math.floor(Math.random() * 1000);
-const randomColor = () =>
-  "#" + Math.floor(Math.random() * 16777215).toString(16);
+const clients = new Set<any>();
 
 export const setupWebSocket = (server: any) => {
   const wss = new WebSocketServer({ server });
 
   wss.on("connection", (ws) => {
-    let currentRoom = "";
-    let client: Client;
+    clients.add(ws);
 
     ws.on("message", (message) => {
       const data = JSON.parse(message.toString());
 
-      if (data.type === "join") {
-        currentRoom = data.room;
-
-        if (!rooms[currentRoom]) {
-          rooms[currentRoom] = new Set();
-        }
-
-        client = {
-          ws,
-          username: randomName(),
-          color: randomColor(),
-        };
-
-        rooms[currentRoom].add(client);
-
-        rooms[currentRoom].forEach((c) => {
-          c.ws.send(
-            JSON.stringify({
-              type: "notification",
-              message: `${client.username} joined`,
-            })
-          );
+      if (data.type === "cursor-move") {
+        clients.forEach((client) => {
+          if (client !== ws) {
+            client.send(JSON.stringify(data));
+          }
         });
       }
     });
 
     ws.on("close", () => {
-      if (currentRoom && rooms[currentRoom]) {
-        rooms[currentRoom].forEach((c) => {
-          if (c.ws === ws) {
-            rooms[currentRoom].delete(c);
-
-            rooms[currentRoom].forEach((c2) => {
-              c2.ws.send(
-                JSON.stringify({
-                  type: "notification",
-                  message: `${c.username} left`,
-                })
-              );
-            });
-          }
-        });
-      }
+      clients.delete(ws);
     });
   });
 };
