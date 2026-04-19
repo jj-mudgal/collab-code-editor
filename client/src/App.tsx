a0684ec6eb4cd3b128af59277ae5001dbe24857e
Janmejai Mudgal <janmejai345@gmail.com>
2026-04-19 08:06:33 +0000

    feat: associate messages with user identity

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,25 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import Editor from "./components/layout/Editor";
-import UserList from "./components/users/UserList";
+import Chat from "./components/chat/Chat";
 import { socket } from "./socket";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [messages, setMessages] = useState<string[]>([]);
-
   useEffect(() => {
-    socket.send(JSON.stringify({ type: "join", room: "room1" }));
+    const username = "user-" + Math.floor(Math.random() * 1000);
 
-    socket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-
-      if (data.type === "users") {
-        setUsers(data.users);
-      }
-
-      if (data.type === "notification") {
-        setMessages((prev) => [...prev, data.message]);
-      }
-    };
+    socket.send(
+      JSON.stringify({
+        type: "join",
+        room: "room1",
+        username,
+      })
+    );
   }, []);
 
   return (
     <div style={{ display: "flex" }}>
-      <UserList users={users} />
-      <div>
-        <Editor />
-        {messages.map((m, i) => (
-          <div key={i}>{m}</div>
-        ))}
-      </div>
+      <Editor />
+      <Chat />
     </div>
   );
 }
