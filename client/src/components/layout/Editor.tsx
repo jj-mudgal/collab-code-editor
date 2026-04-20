745ac2c1c5764ee6e71082a9585be56ebe0412c2
Janmejai Mudgal <janmejai345@gmail.com>
2026-04-20 07:02:32 +0000

    perf: reduce unnecessary editor re-renders

--- client/src/components/layout/Editor.tsx
+++ client/src/components/layout/Editor.tsx
@@ -1,37 +1,31 @@
 import Editor from "@monaco-editor/react";
 import { socket } from "../../socket";
 import { setupCursorManager } from "../../cursorManager";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { debounce } from "../../utils/debounce";
 
 let isRemoteUpdate = false;
 
 const CodeEditor = () => {
   const [code, setCode] = useState("");
-  const [output, setOutput] = useState("");
 
-  const handleMount = (editor: any) => {
+  const handleMount = useCallback((editor: any) => {
     setupCursorManager(editor);
-  };
+  }, []);
 
-  socket.onmessage = (event) => {
-    const data = JSON.parse(event.data);
+  const sendChange = useCallback(
+    debounce((val: string) => {
+      socket.send(
+        JSON.stringify({
+          type: "code-change",
+          code: val,
+        })
+      );
+    }, 300),
+    []
+  );
 
-    if (data.type === "execution-result") {
-      setOutput(data.output);
-    }
-  };
-
-  const sendChange = debounce((val: string) => {
-    socket.send(
-      JSON.stringify({
-        type: "code-change",
-        code: val,
-      })
-    );
-  }, 300);
-
-  const handleChange = (value: string | undefined) => {
+  const handleChange = useCallback((value: string | undefined) => {
     if (isRemoteUpdate) {
       isRemoteUpdate = false;
       return;
@@ -41,30 +35,16 @@ const CodeEditor = () => {
       setCode(value);
       sendChange(value);
     }
-  };
-
-  const runCode = () => {
-    socket.send(
-      JSON.stringify({
-        type: "execute",
-        code,
-        language: "javascript",
-      })
-    );
-  };
+  }, [sendChange]);
 
   return (
-    <div>
-      <button onClick={runCode}>Run</button>
-      <Editor
-        height="70vh"
-        defaultLanguage="javascript"
-        defaultValue="// start coding"
-        onMount={handleMount}
-        onChange={handleChange}
-      />
-      <pre>{output}</pre>
-    </div>
+    <Editor
+      height="90vh"
+      defaultLanguage="javascript"
+      defaultValue="// start coding"
+      onMount={handleMount}
+      onChange={handleChange}
+    />
   );
 };
 
