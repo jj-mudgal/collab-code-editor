97c0452a7184727a5d92b822bde11e22a00d5451
Janmejai Mudgal <janmejai345@gmail.com>
2026-04-25 12:01:31 +0000

    feat: add structured logging for socket events

--- server/src/socket/index.ts
+++ server/src/socket/index.ts
@@ -3,15 +3,18 @@ import { joinRoom, leaveRoom } from "../rooms/roomManager";
 import { subscribe } from "../pubsub/pubsub";
 import { handleMessage } from "./handler";
 import { setupHeartbeat } from "./heartbeat";
-import { validateMessage } from "./validate";
-import { isRateLimited } from "./rateLimit";
+import { log } from "../logger/logger";
 
 export const setupWebSocket = (server: any) => {
   const wss = new WebSocketServer({ server });
 
   setInterval(() => {
     wss.clients.forEach((ws: any) => {
-      if (!ws.isAlive) return ws.terminate();
+      if (!ws.isAlive) {
+        log("warn", "Terminating dead connection");
+        return ws.terminate();
+      }
+
       ws.isAlive = false;
       ws.ping();
     });
@@ -19,24 +22,21 @@ export const setupWebSocket = (server: any) => {
 
   wss.on("connection", (ws: any) => {
     setupHeartbeat(ws);
+    log("info", "New client connected");
+
     let currentRoom = "";
 
     ws.on("message", (message) => {
-      if (isRateLimited(ws)) return;
+      const start = Date.now();
+      const data = JSON.parse(message.toString());
 
-      let data;
-
-      try {
-        data = JSON.parse(message.toString());
-      } catch {
-        return;
-      }
-
-      if (!validateMessage(data)) return;
+      log("info", "Incoming message", { type: data.type });
 
       if (data.type === "join") {
         currentRoom = data.room;
         joinRoom(currentRoom, ws);
+
+        log("info", "User joined room", { room: currentRoom });
 
         subscribe(currentRoom, (msg) => {
           ws.send(JSON.stringify(msg));
@@ -46,9 +46,13 @@ export const setupWebSocket = (server: any) => {
       } else {
         handleMessage(currentRoom, data, ws);
       }
+
+      const latency = Date.now() - start;
+      log("info", "Message processed", { latency });
     });
 
     ws.on("close", () => {
+      log("info", "Client disconnected");
       if (currentRoom) leaveRoom(currentRoom, ws);
     });
   });
